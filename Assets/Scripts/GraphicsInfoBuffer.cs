de138e42c2ac90118a034462f8ed6d2f0ce34f19
castiel-1 <qwrtvbfeuerherz@gmail.com>
2025-03-08 00:43:49 +0100

    comments and letters on wall vfx
    added comments to graphicsInforBuffer and created textOnWall vfx with sdf map of room

--- Assets/Scripts/GraphicsInfoBuffer.cs
+++ Assets/Scripts/GraphicsInfoBuffer.cs
@@ -1,6 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.VFX;
 
+
+/* 
+
+ IF IN SCENE
+- provides the blueprint for letterStructs
+- sets up the graphicsBuffer based on how many letters are supposed to be saved in it
+
+METHODS
+- UpdateBuffer(LetterStruct[]): updates the buffer with changes to all structs in letterStruct[]
+- GetIndex(char): returns findex of char to be used in flipbook
+
+VARIABLES
+- text: holds the text to be displayed
+
+ */
+
+// struct for each letter holding information for displaying it
 [VFXType(VFXTypeAttribute.Usage.GraphicsBuffer)]
 public struct LetterStruct
 {
@@ -12,11 +29,13 @@ public struct LetterStruct
 
 public class GraphicsInfoBuffer : MonoBehaviour
 {
-    public string text = "hello";
+    public string text = "";
     public VisualEffect visualEffect;
     public GraphicsBuffer graphicsBuffer;
+    /*
     public Vector3 startPoint;
     public Vector3 endPoint;
+    */
     public string charSet;
 
     public void Awake()
@@ -24,10 +43,13 @@ public class GraphicsInfoBuffer : MonoBehaviour
         SetUpBuffer();
     }
 
+
+    // create graphics buffer that can hold information for all letters
     public void SetUpBuffer()
     {
         //DEBUG
         Debug.Log("buffer is set up");
+        Debug.Log("Text for displaying:" + text);
 
         int letterCount = text.Length;
         graphicsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, letterCount,
@@ -38,6 +60,7 @@ public class GraphicsInfoBuffer : MonoBehaviour
     }
 
 
+    // updates buffer to display any changes made to letterStructs
     public void UpdateBuffer(LetterStruct[] letterStructs)
     {
         //DEBUG
@@ -55,6 +78,8 @@ public class GraphicsInfoBuffer : MonoBehaviour
         Debug.Log("reloading buffer");
         visualEffect.Reinit();
     }
+
+    // get fIndex of letter for use with the flipbook
     public int GetIndex(char letter)
     {
 
---
+++ Assets/Scripts/TextOnWall.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class TextOnWall : MonoBehaviour
+{
+    public GraphicsInfoBuffer buffer;
+
+    private int letterCount;
+
+    void Start()
+    {
+        letterCount = buffer.text.Length;
+        LetterStruct[] letterStructs = new LetterStruct[letterCount];
+
+        for (int i = 0; i < letterCount; i++)
+        {
+            letterStructs[i] = new LetterStruct
+            {
+                fIndex = buffer.GetIndex(buffer.text[i]),
+                position = Vector3.up,
+                direction = Vector3.up,
+                size = 0.3f,
+            };
+
+        }
+
+        buffer.UpdateBuffer(letterStructs);
+    }
+}
